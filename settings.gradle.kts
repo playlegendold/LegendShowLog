670bd7be23a90034f4acf0edab40408b5c080cb4
Lukas Jost <lukas.jost@outlook.com>
2021-01-10 17:50:27 +0100

    updated gradle (#20)
    updated deps
    updated hooks
    changed to new versioning
    added dependabot
    updated pipelines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,29 @@
-rootProject.name = "legendshowlog"
+rootProject.name = "LegendShowLog"
 include("bukkit")
 
-project(":bukkit").name = "legendshowlog-bukkit"
+project(":bukkit").name = "LegendShowLog-Bukkit"
+
+plugins {
+    id("com.gradle.enterprise").version("3.5.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = false
+    }
+    remote(HttpBuildCache::class) {
+        url = uri("https://repository.playlegend.net/artifactory/cache/")
+        credentials {
+            username = System.getenv("repositoryUser")
+            password = System.getenv("repositoryPassword")
+        }
+        isPush = true;
+    }
+}
